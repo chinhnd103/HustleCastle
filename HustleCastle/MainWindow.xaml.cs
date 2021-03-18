be30c2d446c69c79d652402263941d104604792e
chinhnd103 <chinhnd103@gmail.com>
2021-03-19 00:27:52 +0700

    AddProject
    Add New

---
+++ HustleCastle/MainWindow.xaml.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using KAutoHelper;
+
+namespace HustleCastle
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        List<string> listDevice = ADBHelper.GetDevices();
+        Bitmap D_HOME_MAP = (Bitmap)Bitmap.FromFile("image//dungeon_Tower.jpg");
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            if (listDevice.Count == 0)
+            {
+                MessageBox.Show("No Device!!!!");
+            }
+
+        }
+
+        #region LoadImage
+
+        #endregion
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            //Task t = new Task(()=> {
+            //});
+            CheckImage(listDevice[0], D_HOME_MAP);
+
+            //t.Start();
+        }
+
+        void CheckImage(string id, Bitmap img)
+        {
+            Task l = new Task(() => {
+                while (true)
+                {
+                    var screen = KAutoHelper.ADBHelper.ScreenShoot(id);
+                    var point = KAutoHelper.ImageScanOpenCV.FindOutPoint(screen, img);
+                    if (point != null)
+                    {
+                        Dispatcher.BeginInvoke(new ThreadStart(() => rtb1.AppendText("Home!!!")));
+                        break;
+                    }
+                }
+            });
+            l.Start();
+
+        }
+    }
+}
